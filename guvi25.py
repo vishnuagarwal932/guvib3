a534c7e2227c7a3ee5742fecb69548d51a3aef7c
Vishnu Agarwal <45100624+vishnuagarwal932@users.noreply.github.com>
2018-11-23 21:40:34 +0530

    Create guvi25.py

---
+++ guvi25.py
@@ -0,0 +1,23 @@
+try:
+    n=int(input("enter a no. : "))
+    if(1<=n<=1000):
+        try:
+            x=list(map(int,input().split(' ')))
+            if(n==len(x)):
+                x=sorted(x)
+                if(len(x)%2!=0):
+                    n=(n+1)//2
+                    print(x[n-1])
+                else:
+                    n=n//2
+                    a=float((x[n-1]+x[n])/2)
+                    print(a)
+                    
+            else:
+                print("Invalid input")
+        except ValueError:
+            print("Invalid input")
+    else:
+        print("Invalid input")
+except ValueError:
+    print("Invalid input")
